3957fc84b33d7d5e66ac27d07d289d1896766cc3
upsivakumar <138029660+upsivakumar@users.noreply.github.com>
2023-06-28 21:30:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('🥗Kale, Spianch & Rocket smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
-treamlit.header('🍌 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
